eb155c1e67f945796630f968db80928eadb3cc4b
svanholm200-debug <svanholm200@gmail.com>
2025-11-28 11:59:33 +0100

    Added MemberFeeTests to all kind of members

--- src/MemberFeeTest.java
+++ src/MemberFeeTest.java
@@ -7,7 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberFeeTest {
 
     @Test
-    void calculateMemberFee() {
+    void calculateInactiveMemberFee() {
+        MemberFee fee  = new MemberFee();
+        double result = fee.calculateFee(false, LocalDate.of(1900,12,29));
+        assertEquals(result, 500);
+    }
+    @Test
+    void calculateActiveSeniorMemberFee() {
+        MemberFee fee  = new MemberFee();
+        double result = fee.calculateFee(true, LocalDate.of(2001,12,29));
+        assertEquals(result, 1600);
+    }
+    @Test
+    void calculateActiveJuniorMemberFee() {
+        MemberFee fee  = new MemberFee();
+        double result = fee.calculateFee(true, LocalDate.of(2020,12,29));
+        assertEquals(result, 1000);
+    }
+    @Test
+    void calculateRetireeMemberFee() {
         MemberFee fee  = new MemberFee();
         double result = fee.calculateFee(true, LocalDate.of(1900,12,29));
         assertEquals(result, 1200);
