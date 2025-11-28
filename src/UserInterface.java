f9081530b0824794167b9291c19ef39a68571b14
goegl <goegl12@gmail.com>
2025-11-28 09:39:01 +0100

    Added UserInterface class

---
+++ src/UserInterface.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    private Scanner uISC;
+
+    public UserInterface(Scanner uISC){
+        this.uISC = uISC;
+    }
+
+    public void createUI(){
+        while(true){
+            System.out.println("1. Opret Medlem\n" +
+                                "2. ");
+
+            switch
+        }
+    }
+}
