3e2f898a0213bbc3a296b989bb398fdd256566d7
Mowgli-dk <emiland4@gmail.com>
2025-11-28 10:25:33 +0100

    working on adding switch case in UserInterface class

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,18 +1,40 @@
 import java.util.Scanner;
 
 public class UserInterface {
-    private Scanner uISC;
+    private Scanner inputSC;
+    MemberManager memberList;
 
-    public UserInterface(Scanner uISC){
-        this.uISC = uISC;
+    public UserInterface(Scanner inputSC) {
+        this.inputSC = new Scanner(System.in);
+        this.memberList = new MemberManager();
     }
 
-    public void createUI(){
-        while(true){
-            System.out.println("1. Opret Medlem\n" +
-                                "2. ");
+    public void createUI() {
 
-            switch
+        while (true) {
+            System.out.println("1. Opret Medlem\n" +
+                    "2. ");
+
+
+            int number = inputSC.nextInt();
+            inputSC.nextLine();
+            switch (number) {
+                case 1:
+                    System.out.println("Indtast Navn");
+                    String name = inputSC.nextLine();
+                    System.out.println("Intast nummer");
+                    String phone = inputSC.nextLine();
+                    System.out.println("Intast adresse");
+                    String adress = inputSC.nextLine();
+                    //System.out.println("Intast fødselsdag");
+                    System.out.println("Aktivt medlemsskab?");
+
+                    boolean activeStatus;
+                    if(inputSC.nextInt() == 1){
+                        activeStatus = true;
+                    } else activeStatus = false;
+
+            }
         }
     }
 }
