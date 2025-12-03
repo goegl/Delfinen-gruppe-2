70c43bc3d8336dd1290a36b665899a666231f9bf
svanholm200-debug <svanholm200@gmail.com>
2025-12-03 10:50:18 +0100

    Deleted comment

--- src/CompetitiveResult.java
+++ src/CompetitiveResult.java
@@ -8,7 +8,6 @@ public class CompetitiveResult {
     String competitionName;
 
     public CompetitiveResult(CompetitiveMember member, Disciplines discipline, double resultTime, LocalDate date, String competitionName) {
-        //Lav klasse og tilføj placering i stævnet
         this.member = member;
         this.discipline = discipline;
         this.resultTime = resultTime;
