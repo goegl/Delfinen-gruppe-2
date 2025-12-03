34a8ab0db8fe6452c86c7ed8cbf57bc909a0817b
imaanh <mahadh317@gmail.com>
2025-12-03 09:22:07 +0100

    append problem

--- src/FileManager.java
+++ src/FileManager.java
@@ -6,11 +6,12 @@ public class FileManager {
 
     public void writeMembersToCSV(List<Member> members, String filepath){
         try(FileWriter writer = new FileWriter(filepath, true)){
-            writer.append("Name, PhoneNumber, Address"+ "\n");
+            writer.append("Name, PhoneNumber, Address, Aktiv Status, Fødselsdagsdato, MedlemsId, Kontingentsats, Oprettelsesdato"+ "\n");
             for(Member member : members){
                 writer.append(member.getName()).append(",");
                 writer.append(member.getPhoneNumber()).append(",");
                 writer.append(member.getAddress()).append(",");
+                writer.append(member.isActiveStatus()).append(",");
             }
             writer.close();
             System.out.println("CSV fil skrevet til" + filepath);
