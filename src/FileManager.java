5539974a873f0d8c94bc6235e0ccb33b2fd8d6d7
goegl <goegl12@gmail.com>
2025-12-04 09:36:33 +0100

    removed unnecessary braces

--- src/FileManager.java
+++ src/FileManager.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class FileManager {
 
-    public void writeMembersToCSV(List<Member> members, String filepath){
-        try(FileWriter writer = new FileWriter(filepath, true)){
-            writer.append("Name, PhoneNumber, Address, Aktiv Status, Fødselsdagsdato, MedlemsId, Kontingentsats, Oprettelsesdato"+ "\n");
-            for(Member member : members){
+    public void writeMembersToCSV(List<Member> members, String filepath) {
+        try (FileWriter writer = new FileWriter(filepath, true)) {
+            writer.append("Name, PhoneNumber, Address, Aktiv Status, Fødselsdagsdato, MedlemsId, Kontingentsats, Oprettelsesdato" + "\n");
+            for (Member member : members) {
                 writer.append(member.getName()).append(" ,");
                 writer.append(member.getPhoneNumber()).append(" ,");
                 writer.append(member.getAddress()).append(" ,");
@@ -21,12 +21,13 @@ public class FileManager {
             }
             writer.close();
             System.out.println("CSV fil skrevet til" + filepath);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-    }}
     }
-
 }
+
+
+
 
 
