f18d582f32d675ba7aef161f9e44edb6b23c89dd
goegl <goegl12@gmail.com>
2025-12-08 09:45:01 +0100

    Added create results methods in ResultManager.java

--- src/ResultManager.java
+++ src/ResultManager.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +9,14 @@ public class ResultManager {
     public ResultManager() {
         this.competitiveResults = new ArrayList<>();
         this.trainingResults = new ArrayList<>();
+    }
+
+    public TrainingResult createTrainingResult(CompetitiveMember member, Disciplines discipline, double resultTime,
+                                               LocalDate date){
+        return new TrainingResult(member, discipline, resultTime, date);
+    }
+    public CompetitiveResult createCompetitiveResult(CompetitiveMember member, Disciplines discipline,
+                                                          double resultTime, LocalDate date, String competitionName){
+        return new CompetitiveResult(member, discipline, resultTime, date, competitionName);
     }
 }
