c47b2869a02f9e543bb7a60fbef697a6cf41a63b
goegl <goegl12@gmail.com>
2025-12-08 09:47:01 +0100

    Adjusted ResultManager.java creation methods to also add to the list

--- src/ResultManager.java
+++ src/ResultManager.java
@@ -13,10 +13,14 @@ public class ResultManager {
 
     public TrainingResult createTrainingResult(CompetitiveMember member, Disciplines discipline, double resultTime,
                                                LocalDate date){
-        return new TrainingResult(member, discipline, resultTime, date);
+        TrainingResult trainingResult =  new TrainingResult(member, discipline, resultTime, date);
+        trainingResults.add(trainingResult);
+        return trainingResult;
     }
     public CompetitiveResult createCompetitiveResult(CompetitiveMember member, Disciplines discipline,
                                                           double resultTime, LocalDate date, String competitionName){
-        return new CompetitiveResult(member, discipline, resultTime, date, competitionName);
+        CompetitiveResult competitiveResult = new CompetitiveResult(member, discipline, resultTime, date, competitionName);
+        competitiveResults.add(competitiveResult);
+        return competitiveResult;
     }
 }
